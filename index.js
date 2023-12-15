36b709341b53e2fb0da9e473665aba7fb93ece9d
tabitha muriithi <excellentwriter120@gmail.com>
2023-12-15 13:21:49 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+let c = 2000
+let d = 100
+
+const divide = (c, d) => c/d
+   console.log(divide(c,d));
+
+
+let e = 2
+const square = (e) => e*e;
+   
+    console.log(square(e));
+
+
+let a = 3
+let b = 4
+const add = (a, b) => a + b
+
+
+    console.log(add(a,b));
+
+
